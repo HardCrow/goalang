6d7bea72f15e6b8b44c66d9e6d8dd638cc34d104
Huang <Huang@test.com>
2023-07-30 20:57:10 +0800

    日常提交

---
+++ src/go_code/routine/perject14/a/address.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	//结构体赋值方式一
+	p2 := person{"huang", 22}
+	//结构体赋值方式二
+	var p1 *person = new(person)
+	p1.Name = "js" //p1.name="xx" 底层就是(*p1).Name="xx"的形式
+	//(*p1).Name="li"
+	(*p1).Age = 20
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
