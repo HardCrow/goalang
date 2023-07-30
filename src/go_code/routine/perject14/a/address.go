5219c9d6c1c029a54c4fad613de447a455a560be
Huang <Huang@test.com>
2023-07-30 20:57:47 +0800

    日常提交

--- src/go_code/routine/perject14/a/address.go
+++ src/go_code/routine/perject14/a/address.go
@@ -12,7 +12,7 @@ func main() {
 	p2 := person{"huang", 22}
 	//结构体赋值方式二
 	var p1 *person = new(person)
-	p1.Name = "js" //p1.name="xx" 底层就是(*p1).Name="xx"的形式
+	p1.Name = "js" //p1.name="xx" 底层就是(*p1).Name="xx"的形式 前提是必须 var p1 *person =new （person）
 	//(*p1).Name="li"
 	(*p1).Age = 20
 
